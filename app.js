fff8777107a6505b530adbf64612dfde6a84e647
Ian Hancock <northroadian@gmail.com>
2021-10-13 05:46:16 -0500

    weather app js

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const dateOutput = document.querySelector(".date")
 const timeOutput = document.querySelector(".time")
 const conditionOutput = document.querySelector(".condition")
 const nameOutput = document.querySelector(".name")
+const icon = document.querySelector(".icon")
+const cloudOutput = document.querySelector(".cloud")
+const humidityOutput = document.querySelector(".humidity")
+const windOutput = document.querySelector(".wind")
+const form = document.getElementById('.locationInput')
+const search = document.querySelector(".search")
+const btn = document.querySelector(".submit")
+const cities = document.querySelector(".city")
+
+// Default city when page loads 
+let cityInput = "Nikiski"
+//Add the click event to each city in the panel
+cities.forEach((city)=> {
+    city.addEventListener('click',(e)=>{
+        cityInput = e.target.innerHtml 
+// this is the function below that fetches the weather data from the api call.
+        fetchWeatherData()
+    })
+})
