739a8e3dee4700075754631d7e829c5b18799965
Ian Hancock <northroadian@gmail.com>
2021-10-12 22:31:31 -0500

    query selectors

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-window.addEventListener('load', ()=> {
-    let long; 
-    let lat; 
-    if(navigator.geolocation){
-        return navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude
-            lat = position.coords.latitude
-
-            const api = `api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}${lat},${long}`;
-            fetch(api)
-            .then(response =>{
-               return response.json()
-            })
-           .then(data => {
-               console.log(data)
-           })
-        });
-  
-    }
-})
+const app = document.querySelector(".weather-app")
+const temp = document.querySelector(".temp")
+const dateOutput = document.querySelector(".date")
+const timeOutput = document.querySelector(".time")
+const conditionOutput = document.querySelector(".condition")
+const nameOutput = document.querySelector(".name")
